0ffb9449b82c891c3491e153681e24333290af66
BryanJavierGarciaBarrios <145368570+BryanJavierGarciaBarrios@users.noreply.github.com>
2023-10-30 14:03:19 -0600

    Busqueda_de_vuelta_atras

---
+++ 002_Busqueda_de_vuelta_atras.py
@@ -0,0 +1,51 @@
+def is_valid(board, row, col, num):
+    # Verifica si el número es válido en la fila
+    if num in board[row]:
+        return False
+
+    # Verifica si el número es válido en la columna
+    if num in [board[i][col] for i in range(9)]:
+        return False
+
+    # Verifica si el número es válido en el cuadro 3x3
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(start_row, start_row + 3):
+        for j in range(start_col, start_col + 3):
+            if board[i][j] == num:
+                return False
+
+    return True
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[row][col] = 0
+
+                return False
+    return True
+
+# Ejemplo de un tablero de Sudoku (0 representa una casilla vacía)
+sudoku_board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+if solve_sudoku(sudoku_board):
+    print("Solución del Sudoku:")
+    for row in sudoku_board:
+        print(row)
+else:
+    print("No se encontró una solución para el Sudoku.")
